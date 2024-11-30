167bc07c320f5ac0bbc26b1f9115b0139d890d4e
WillowRussia <iv040606@gmail.com>
2024-11-30 16:25:28 +0300

    Initial Commit

---
+++ VKTestApp/VKTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKTestAppApp.swift
+//  VKTestApp
+//
+//  Created by Илья Востров on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct VKTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
